a516140f166707bf558b11cf3468843f65f786ed
Charlie Mac <coding.0awnx@aleeas.com>
2022-03-12 15:30:43 +1000

    capture input field value and search movie API

--- movieSearch/script.js
+++ movieSearch/script.js
@@ -1,12 +1,15 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com", {
     params: {
       apikey: "7b4e2867",
-      s: "avengers",
+      s: searchTerm,
     },
   });
 
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+input.addEventListener("input", (event) => {
+  fetchData(event.target.value);
+});
